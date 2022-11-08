69028453d9f20e4e88f1fd4037c18d41e4c61160
sfinksik12 <vasya1pupkin12453@gmail.com>
2022-11-08 12:59:43 +0300

    stable

--- playwright.config.js
+++ playwright.config.js
@@ -7,9 +7,6 @@ const { devices } = require('@playwright/test');
  */
 const config = {
   testDir: './tests',
-  testMatch: [
-    "./tests/ddt_add_company_users.spec.js"
-  ],
   timeout: 40000,
   expect: {
     timeout: 10000
@@ -18,20 +15,11 @@ const config = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? [["junit", {
-    outputFile: "results.xml"
-  }]] : [["json", {
-    outputFile: "report.json"
-  }], ["html", {
-    open: "on-failure"
-  }]],
+  reporter: [ ['html'] ],
   use: {
     viewport: { width: 1920, height: 1080 },
     actionTimeout: 0,
-    headless: process.env.CI ? true : false,
-    trace: process.env.CI ? "off" : "on",
-    video: process.env.CI ? "off" : "on",
-    screenshot: process.env.CI ? "off" : "on",
+    headless: false,  
   },
 
 
